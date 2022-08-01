3578e9afafef6e4985c342ccf71365c5b455c75e
Jareer Abdullah <jforeverything2007@gmail.com>
2022-08-01 23:05:29 +0200

    asd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,9 +6,9 @@
 mod presence;
 
 fn main() {
+    presence::set_presence();
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("Unknown Error Occurred While Running Application!");
-    let discord = presence::set_presence();
-    println!("{:?}", discord);
+    loop {}
 }
--- src-tauri/src/presence.rs
+++ src-tauri/src/presence.rs
@@ -1,22 +1,26 @@
-use discord_rich_presence::{
-    activity::{self, Assets, Button},
-    DiscordIpc, DiscordIpcClient,
-};
+use discord_presence::Client as DiscordRPC;
 
-pub fn set_presence() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = DiscordIpcClient::new("1002568032041832578")?;
+pub fn set_presence() {
+    let mut drpc = DiscordRPC::new(1002568032041832578);
 
-    client.connect()?;
-    client.set_activity(
-        activity::Activity::new()
-            .details("Creating a Discord bot.")
-            .assets(Assets::large_image(Assets::new(), "1024x1024"))
-            .assets(Assets::small_image(Assets::new(), "512x512"))
-            .buttons(vec![Button::new(
-                "Learn more",
-                "https://github.com/jareer12/hydrazine",
-            )]),
-    )?;
+    drpc.on_ready(|_ctx| {
+        println!("READY!");
+    });
 
-    loop {}
+    drpc.on_error(|ctx| {
+        eprintln!("An error occured, {}", ctx.event);
+    });
+
+    drpc.start();
+
+    if let Err(why) = drpc.set_activity(|a| {
+        a.state("Running examples").assets(|ass| {
+            ass.large_image("1024x1024")
+                .large_text("Building a Discord Bot")
+                .small_image("512x512")
+                .small_text("With Hydrazine")
+        })
+    }) {
+        println!("Failed to set presence: {}", why);
+    }
 }
